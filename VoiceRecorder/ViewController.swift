83498e3aba8c574d28d7c14762dc694259507e7c
이경민 <cow970814@naver.com>
2022-06-27 14:58:58 +0900

    <chore> Firebase Storage 추가 및 FirebaseApp init

--- VoiceRecorder/ViewController.swift
+++ VoiceRecorder/ViewController.swift
@@ -6,12 +6,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("Test Number 1")
     }
-
-
 }
-
