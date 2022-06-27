c07a9165afda317855e9f43bf5974e174d2a0490
신의연 <dusdl14@gmail.com>
2022-06-27 15:03:42 +0900

    create entry point of Firebase

--- VoiceRecorder/AppDelegate.swift
+++ VoiceRecorder/AppDelegate.swift
@@ -4,14 +4,17 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure() // Firebase entry point
+        
         return true
     }
 
