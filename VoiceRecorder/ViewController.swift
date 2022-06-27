27ab60a854eadc0567b512efa433bb714e81f621
이경민 <cow970814@naver.com>
2022-06-27 16:07:21 +0900

    <chore>add Example Label

--- VoiceRecorder/ViewController.swift
+++ VoiceRecorder/ViewController.swift
@@ -7,9 +7,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    lazy var titleLabel:UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .gray
+        label.textColor = .white
+        label.text = "Example"
+        label.textAlignment = .center
+        label.layer.cornerRadius = 20
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
         
-        print("Test Number 1")
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        titleLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
     }
 }
