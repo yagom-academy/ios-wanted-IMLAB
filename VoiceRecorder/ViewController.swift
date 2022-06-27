ff6b45643039c8506121e269d156bd0c2d817ec4
이경민 <cow970814@naver.com>
2022-06-27 22:17:15 +0900

    <feat> SceneDelegate UINavigationController 설정 <feat> RecordViewController 생성 <feat> Voice Recoder 권환 설정 <feat> Voice Recoder 설정

--- VoiceRecorder/ViewController.swift
+++ VoiceRecorder/ViewController.swift
@@ -7,25 +7,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var titleLabel:UILabel = {
-        let label = UILabel()
-        label.backgroundColor = .gray
-        label.textColor = .white
-        label.text = "Example"
-        label.textAlignment = .center
-        label.layer.cornerRadius = 20
-        return label
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(titleLabel)
         
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        titleLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        titleLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        setupNabBar()
+    }
+    
+    func setupNabBar(){
+        let rightButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(presentRecordPage))
+        navigationItem.rightBarButtonItem = rightButton
+        
+        navigationItem.title = "Table View"
+    }
+    
+    @objc func presentRecordPage(){
+        let rootViewController = RecordViewController()
+        self.present(rootViewController, animated: true)
     }
 }
