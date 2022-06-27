ddcd45e2f2144fd564be55049ea6c93b0713051e
jmindevelper <jmin.developer@gmail.com>
2022-06-27 17:59:19 +0900

    [feat] firebase 설치 #7

--- VoiceRecorder/Application/AppDelegate.swift
+++ VoiceRecorder/Application/AppDelegate.swift
@@ -4,6 +4,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -12,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
