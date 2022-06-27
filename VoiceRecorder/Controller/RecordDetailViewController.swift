9350a7113f16f14481d7f1f5b603bc8540c395da
bhoon-coding <bhooncoding@gmail.com>
2022-06-27 19:25:04 +0900

    [Design] 두번째 화면 IBOutlet, IBAction 세팅

--- VoiceRecorder/Controller/RecordDetailViewController.swift
+++ VoiceRecorder/Controller/RecordDetailViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class RecordDetailViewController: UIViewController {
-
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var recordWaveView: UIView!
+    @IBOutlet weak var cutoffLabel: UILabel!
+    @IBOutlet weak var recordProgressBar: UISlider!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    // MARK: - Properties
+    
+    
+    
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBActions
+    
+    @IBAction func controlRecordProgressBar(_ sender: UISlider) {
     }
-    */
-
+    
+    @IBAction func controlRecordButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func controlBackButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func controlPlayButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func controlNextButton(_ sender: UIButton) {
+    }
+    
+    
 }
+// MARK: - Extensions
