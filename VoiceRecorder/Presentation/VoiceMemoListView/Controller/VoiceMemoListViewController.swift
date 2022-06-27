0ce3419b5f4653ffaf770b41a1dc492745f5a534
dahun-lee-daji <ssss6596@gmail.com>
2022-06-27 16:25:08 +0900

    chore: 프로젝트 폴더 구성 #4
    chore: Main 스토리보드 삭제.

--- VoiceRecorder/Presentation/VoiceMemoListView/Controller/VoiceMemoListViewController.swift
+++ VoiceRecorder/Presentation/VoiceMemoListView/Controller/VoiceMemoListViewController.swift
@@ -5,13 +5,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VoiceMemoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
