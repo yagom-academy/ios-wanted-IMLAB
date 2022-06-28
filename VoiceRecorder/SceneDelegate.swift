761f2952bf18d39527a893c6774243dd12642add
이경민 <cow970814@naver.com>
2022-06-28 23:40:59 +0900

    [feat] RecordViewController 녹음,재생,녹음완료,일시정지 메소드 구현

--- VoiceRecorder/SceneDelegate.swift
+++ VoiceRecorder/SceneDelegate.swift
@@ -16,9 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        let rootViewController = ViewController()
+        window.rootViewController = UINavigationController(rootViewController: rootViewController)
         window.makeKeyAndVisible()
         self.window = window
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
