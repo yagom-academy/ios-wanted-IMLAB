1ade11bfb6b9f44d988c2f0cdc8940a340901efb
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-06-28 17:29:03 +0900

    [style] layout plusButton

--- VoiceRecorder/ViewController.swift
+++ VoiceRecorder/ViewController.swift
@@ -16,8 +16,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        setLayout()
+        
         plusButton.addTarget(self, action: #selector(plusButtonClicked), for: .touchUpInside)
+    }
+    
+    func setLayout() {
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(plusButton)
+        
+        NSLayoutConstraint.activate([
+            plusButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            plusButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            plusButton.widthAnchor.constraint(equalToConstant: 80),
+            plusButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     @objc func plusButtonClicked() {
