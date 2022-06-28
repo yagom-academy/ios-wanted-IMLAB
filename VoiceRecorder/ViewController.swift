96926c178dd3db099a3d9449fdb277d3c91982c0
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-06-28 17:00:28 +0900

    [style] plusButton 생성

--- VoiceRecorder/ViewController.swift
+++ VoiceRecorder/ViewController.swift
@@ -7,11 +7,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var plusButton: UIButton = {
+        var button = UIButton()
+        button.setTitle("plus", for: .normal)
+        button.backgroundColor = .blue
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
