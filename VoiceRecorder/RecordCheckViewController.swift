e1ec5e7fef5ecef5305e1cd9bfc5c83ecc528590
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-06-28 16:23:35 +0900

    [feat] Add RecordCheckVC

---
+++ VoiceRecorder/RecordCheckViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordCheckViewController.swift
+//  VoiceRecorder
+//
+//  Created by 백유정 on 2022/06/28.
+//
+
+import UIKit
+
+class RecordCheckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
