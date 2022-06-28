893b55266844a429e5976d35f799c0812dc8b48e
jmindevelper <jmin.developer@gmail.com>
2022-06-28 15:44:05 +0900

    [feat] VoiceMemoPlayViewController 생성

---
+++ VoiceRecorder/Presentation/VoiceMemoPlayView/Controller/VoiceMemoPlayViewController.swift
@@ -0,0 +1,21 @@
+//
+//  VoiceMemoPlayViewController.swift
+//  VoiceRecorder
+//
+//  Created by J_Min on 2022/06/28.
+//
+
+import UIKit
+
+class VoiceMemoPlayViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+}
+VoiceRecorder/Application/SceneDelegate.swift
+VoiceRecorder.xcodeproj/project.pbxproj
+VoiceRecorder/Presentation/VoiceMemoPlayView/Controller/VoiceMemoPlayViewController.swift
