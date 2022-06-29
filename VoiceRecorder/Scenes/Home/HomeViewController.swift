7886a8154d849ca2c3e132edd55f012fe17a39d4
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-06-29 20:13:40 +0900

    [#14] 네비게이션바 버튼, 액션구현

--- VoiceRecorder/Scenes/Home/HomeViewController.swift
+++ VoiceRecorder/Scenes/Home/HomeViewController.swift
@@ -9,7 +9,23 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationBar()
     }
 
+}
+
+private extension HomeViewController {
+    
+    func setNavigationBar() {
+        title = "Voice Memos"
+        let audioCreationButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAddButton))
+        navigationItem.rightBarButtonItem = audioCreationButton
+    }
+    
+    @objc func didTapAddButton() {
+        //TODO: Push AudioCreation VC
+        print("clicked!")
+    }
+    
 }
 
