d9324f8163fcfdc2fbde28dd102f70d59035b0c8
신의연 <dusdl14@gmail.com>
2022-06-29 10:57:24 +0900

    Add custom NSLayoutConstraint
    오토레이아웃을 위한 custom multiplier 추가
    공용 파일로 전체에서 사용할 수 있도록 extenstion 추가

---
+++ VoiceRecorder/PlayRecordedVoice/VoicePlayingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VoicePlayingViewController.swift
+//  VoiceRecorder
+//
+//  Created by 신의연 on 2022/06/29.
+//
+
+import UIKit
+
+class VoicePlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
