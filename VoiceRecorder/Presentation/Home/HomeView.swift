722f49eb9da3538824643b6354725d5f00099984
chansenbbai <chanchu@hansung.ac.kr>
2022-06-29 13:18:03 +0900

    [#1] 프로젝트 초기 세팅
    ### Description
    
    -  폴더링
    - SPM을 통한 Firebase Storage 추가
     - SceneDelegate 설정
    
    Resolve #1

---
+++ VoiceRecorder/Presentation/Home/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  VoiceRecorder
+//
+//  Created by 김승찬 on 2022/06/29.
+//
+
+import UIKit
+
+final class HomeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setupView() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+}
---
+++ VoiceRecorder/Presentation/Recording/RecordingView.swift
@@ -0,0 +1,32 @@
+//
+//  RecordingView.swift
+//  VoiceRecorder
+//
+//  Created by 김승찬 on 2022/06/29.
+//
+
+import UIKit
+
+final class RecordingView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setupView() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+    
+    
+}
