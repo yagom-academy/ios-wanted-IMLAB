823ffbb7e3f23f2e36e2e007278c0f6eda08b9e8
Kim-Junhwan <tazanboy1@naver.com>
2022-06-29 15:58:07 +0900

    chore : add playButton at PlayRecordView
    -PlayVoiceView에 Play버튼 추가

--- VoiceRecorder/Controller/PlayVoiceViewController.swift
+++ VoiceRecorder/Controller/PlayVoiceViewController.swift
@@ -11,20 +11,33 @@ class PlayVoiceViewController: UIViewController {
     
     var voiceRecordViewModel : VoiceRecordViewModel!
 
+    var playAndPauseButton: UIButton = {
+        let playAndPauseButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        playAndPauseButton.translatesAutoresizingMaskIntoConstraints = false
+        playAndPauseButton.setImage(UIImage(systemName: "play"), for: .normal)
+        playAndPauseButton.setPreferredSymbolConfiguration(.init(pointSize: 50), forImageIn: .normal)
+        playAndPauseButton.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+        return playAndPauseButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setView()
+        autolayOut()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setView(){
+        self.view.addSubview(playAndPauseButton)
     }
-    */
-
+    
+    func autolayOut(){
+        NSLayoutConstraint.activate([
+            playAndPauseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            playAndPauseButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+    
+    @objc func tapButton(){
+        print("tap button")
+    }
 }
