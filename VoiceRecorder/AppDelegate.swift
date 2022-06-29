248b93a1508028e84f05528b581ea70b3e2b9ea5
3dots3craters <3dots3craters@gmail.com>
2022-06-29 17:55:24 +0900

    feat : set audioCategory and request mic permission
    - 사용할 오디오 카테고리 설정(playAndRecord)
    - 마이크 사용 권한 요청

--- VoiceRecorder/AppDelegate.swift
+++ VoiceRecorder/AppDelegate.swift
@@ -5,6 +5,7 @@
 
 import UIKit
 import FirebaseCore
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        // Override point for customization after application launch.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playAndRecord)
+            audioSession.requestRecordPermission{ accepted in
+                if accepted {
+                    print("permission granted")
+                }
+            }
+        } catch {
+            print("audioSession error: \(error.localizedDescription)")
+        }
+
         return true
     }
 
