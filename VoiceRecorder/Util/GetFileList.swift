06d9f1f47f8ba11de9fc9dd90ba5c425dbc4e06a
JangJumyeong <isouvezz@gmail.com>
2022-06-29 12:29:04 +0900

    feat : firebase voiceRecords 폴더 파일 리스트 가져오기 기능 생성

---
+++ VoiceRecorder/Util/GetFileList.swift
@@ -0,0 +1,32 @@
+//
+//  GetFileList.swift
+//  VoiceRecorder
+//
+//  Created by 장주명 on 2022/06/29.
+//
+
+import Foundation
+import FirebaseStorage
+
+class GetFileList : FirebaseStoreFileList {
+    
+    func getFirebaseStoreFileList() -> [String] {
+        
+        var filePaths = [String]()
+        let storageReference = Storage.storage().reference().child("voiceRecords")
+        storageReference.listAll { result, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let datas = result else { return }
+            
+            for data in datas.items {
+                filePaths.append(data.fullPath)
+            }
+        }
+    }
+    
+    
+}
