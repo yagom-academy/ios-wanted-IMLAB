bbd1ed453d44c0d7d35989fb64e4575b16347534
yoonju lee <rsyjlee@gmail.com>
2022-06-29 15:11:43 +0900

    [#4] Model 타입 생성

---
+++ VoiceRecorder/Domain/Entity/AudioInformation.swift
@@ -0,0 +1,14 @@
+//
+//  AudioInformation.swift
+//  VoiceRecorder
+//
+//  Created by 이윤주 on 2022/06/29.
+//
+
+import Foundation
+
+struct AudioInformation {
+
+    let name: String
+    let duration: Double
+}
