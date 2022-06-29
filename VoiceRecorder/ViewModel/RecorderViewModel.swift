f48568335c95cfdd48a8611a7bb252b7e2f4dfd5
3dots3craters <3dots3craters@gmail.com>
2022-06-29 14:48:23 +0900

    feat : add RecorderViewModel file
    - 녹음 기능을 담당하는 RecorderViewModel 생성

---
+++ VoiceRecorder/ViewModel/RecorderViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  RecorderViewModel.swift
+//  VoiceRecorder
+//
+//  Created by hayeon on 2022/06/29.
+//
+
+import UIKit
+import AVFAudio
+
+struct RecorderViewModel{
+    
+    private var recorder : AVAudioRecorder?
+    
+    init(_ url : URL){
+        self.recorder = nil // 초기화
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        do {
+            let recorder = try AVAudioRecorder(url: url, settings: settings)
+            self.recorder = recorder
+            // AVAudioRecorder가 throw를 할 수 있기 때문에 do 사용
+        }
+        catch {
+            print("error")
+        }
+    }
+    
+    func startRecording() {
+        self.recorder?.prepareToRecord() //init보다는 startRecording 쪽으로 묶이는 게 더 자연스러워 보여서
+        self.recorder?.record()
+    }
+    
+    func stopRecording() {
+        self.recorder?.stop()
+        // 파일 업로드
+    }
+}
