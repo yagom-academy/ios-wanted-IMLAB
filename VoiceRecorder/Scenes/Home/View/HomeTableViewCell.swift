d8d1fedcd9f479cce988bf1f45715c6b710aa105
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-06-29 20:38:59 +0900

    [#13,#15] 테이블뷰, dataSource, Cell 구현

---
+++ VoiceRecorder/Scenes/Home/View/HomeTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  HomeTableViewCell.swift
+//  VoiceRecorder
+//
+//  Created by Kai Kim on 2022/06/29.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    
+    static let id = "HomeTableViewCell"
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setConstraints()
+    }
+    
+    private var title: UILabel = {
+        let label = UILabel()
+        label.font = .mediumRegular
+        label.text = "2022.04.22:20:22:11"
+        return label
+    }()
+    
+    private var length: UILabel = {
+        let label = UILabel()
+        label.font = .mediumRegular
+        label.text = "12:33"
+        return label
+    }()
+    
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [title, length])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private func setConstraints() {
+        contentView.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ VoiceRecorder/Scenes/Home/View/HomeViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  VoiceRecorder
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+    var homeTableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBar()
+        setTableView()
+        setConstraints()
+    }
+
+}
+
+private extension HomeViewController {
+    
+    func setNavigationBar() {
+        title = "Voice Memos"
+        let audioCreationButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAddButton))
+        navigationItem.rightBarButtonItem = audioCreationButton
+    }
+    
+    @objc func didTapAddButton() {
+        //TODO: Push AudioCreation VC
+        print("clicked!")
+    }
+    
+    func setTableView(){
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.id)
+        homeTableView = tableView
+        homeTableView?.translatesAutoresizingMaskIntoConstraints = false
+        homeTableView?.delegate = self
+        homeTableView?.dataSource = self
+    }
+    
+    func setConstraints() {
+        guard let homeTableView = homeTableView else {return}
+        view.addSubview(homeTableView)
+        NSLayoutConstraint.activate([
+            homeTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            homeTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            homeTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            homeTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+}
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.id) as? HomeTableViewCell else {return UITableViewCell()}
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return CGFloat(50)
+//    }
+}
