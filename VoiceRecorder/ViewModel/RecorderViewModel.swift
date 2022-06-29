2995c922c36a588473f2e2dc1b176eb389fc0477
3dots3craters <3dots3craters@gmail.com>
2022-06-29 18:03:25 +0900

    refactor : change RecorderViewModel content
    - 녹음 저장 파일명 만드는 코드 간결화
    - prepareToRecord 메서드 init에서 실행
      - 녹음 버튼 클릭과 실제 녹음 간 딜레이 최소화 목적

--- VoiceRecorder/ViewModel/RecorderViewModel.swift
+++ VoiceRecorder/ViewModel/RecorderViewModel.swift
@@ -6,32 +6,32 @@
 //
 
 import UIKit
-import AVFAudio
+import AVFoundation
 
 struct RecorderViewModel{
     
     private var recorder : AVAudioRecorder?
     
-    init(_ url : URL){
+    init(){
         self.recorder = nil // 초기화
-        let settings = [
+        let soundFileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent("myRecoding.m4a")
+        let recordSettings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
             AVSampleRateKey: 12000,
             AVNumberOfChannelsKey: 1,
             AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-        ]
+          ]
+        
         do {
-            let recorder = try AVAudioRecorder(url: url, settings: settings)
-            self.recorder = recorder
-            // AVAudioRecorder가 throw를 할 수 있기 때문에 do 사용
-        }
-        catch {
-            print("error")
+            try recorder = AVAudioRecorder(url: soundFileURL, settings: recordSettings)
+            recorder?.prepareToRecord()
+        } catch {
+            print("audioSession Error: \(error.localizedDescription)")
         }
     }
     
     func startRecording() {
-        self.recorder?.prepareToRecord() //init보다는 startRecording 쪽으로 묶이는 게 더 자연스러워 보여서
         self.recorder?.record()
     }
     
@@ -41,6 +41,6 @@ struct RecorderViewModel{
     }
     
     func isRecording() -> Bool {
-        self.recorder?.isRecording ?? false
+        return self.recorder?.isRecording ?? false
     }
 }
