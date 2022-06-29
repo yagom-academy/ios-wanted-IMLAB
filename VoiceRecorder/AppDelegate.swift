fd7bc23662bc87150028fefb75fc5c285347b59f
JangJumyeong <isouvezz@gmail.com>
2022-06-29 10:19:39 +0900

    chore : 누락된 Firebase Info.plish 추가 및 초기화

--- VoiceRecorder/AppDelegate.swift
+++ VoiceRecorder/AppDelegate.swift
@@ -5,7 +5,6 @@
 
 import UIKit
 import FirebaseCore
-import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
