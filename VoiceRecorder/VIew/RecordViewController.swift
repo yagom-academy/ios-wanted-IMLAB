e85f51e87b80f5fa37b575f6e35889cce47438a1
이경민 <cow970814@naver.com>
2022-06-30 12:26:42 +0900

    feat : Audio Engine 5초 전후 이동 추가
    녹음 파일 재생시 5초 전후 이동 메소드 추가

--- VoiceRecorder/VIew/RecordViewController.swift
+++ VoiceRecorder/VIew/RecordViewController.swift
@@ -21,7 +21,6 @@ class RecordViewController:UIViewController{
     
     public private(set) var isRecording = false
     private var audioEngine: AudioEngine!
-    var engine:AudioEngine?
     
     lazy var recordButton:UIButton = {
         let button = UIButton()
@@ -89,10 +88,14 @@ class RecordViewController:UIViewController{
         
     }
     @objc func previusSec(){
-        print("pre Tapped")
+        print("Tapped prev")
+        audioEngine.checkEngineRunning()
+        audioEngine.skip(forwards: false)
     }
     @objc func nextSec(){
-        print("next Tapped")
+        print("Tapped next")
+        audioEngine.checkEngineRunning()
+        audioEngine.skip(forwards: true)
     }
     @objc func playPause(_ sender:UIButton){
         audioEngine.checkEngineRunning()
