07e7f1b79fc2a845473b95df4913f58c2c257a54
wooooozin <wooooozin@gmail.com>
2022-07-01 15:00:42 +0900

    feat: PlayView 각 버튼 기능 구현

--- VoiceRecorder/Controllers/PlayViewController.swift
+++ VoiceRecorder/Controllers/PlayViewController.swift
@@ -11,9 +11,11 @@ class PlayViewController: UIViewController {
     @IBOutlet weak var dateTitleLabel: UILabel!
     @IBOutlet weak var voiceChangeSegment: UISegmentedControl!
     @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var playButton: UIButton!
     
     var recordFile: RecordModel?
     var player: AudioPlayer?
+    var isPlay = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +24,22 @@ class PlayViewController: UIViewController {
     }
     
     @IBAction func didTapPlayBack5Button(_ sender: UIButton) {
-        
+        player?.seek(-5)
     }
     
     @IBAction func didTapPlayForward5Button(_ sender: UIButton) {
-        
+        player?.seek(5)
     }
     
     @IBAction func didTapPlayPauseButton(_ sender: UIButton) {
-        player?.play()
+        if isPlay {
+            sender.setImage(Icon.play.image, for: .normal)
+            player?.stop()
+        } else {
+            sender.setImage(Icon.pauseFill.image, for: .normal)
+            player?.play()
+        }
+        isPlay = !isPlay
     }
     
     func configureUI() {
@@ -41,6 +50,11 @@ class PlayViewController: UIViewController {
     func setUpPlayer() {
         guard let recordFile = recordFile else { return }
         player = recordFile.audioPlayer
+        player?.didFinish = {
+            self.player?.stop()
+            self.playButton.setImage(Icon.play.image, for: .normal)
+            self.isPlay = false
+        }
     }
 
 }
