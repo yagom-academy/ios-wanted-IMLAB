157f1a8d10418b5589eb527d2b38ebbb8d86a0a0
seocho <whtjdqls617@gmail.com>
2022-07-02 21:27:02 +0900

    [Refactor]: print 삭제

--- VoiceRecorder/Controller/VoiceMemoViewController.swift
+++ VoiceRecorder/Controller/VoiceMemoViewController.swift
@@ -105,7 +105,6 @@ extension VoiceMemoViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            print(localUrls[indexPath.row])
             FireStorageManager.shared.deleteItem(fileNames[indexPath.row])
             do {
                 try FileManager.default.removeItem(at: localUrls[indexPath.row])
