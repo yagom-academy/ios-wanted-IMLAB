eafe56a1ae3f1173c0cccec14438ccfd318533c9
신의연 <dusdl14@gmail.com>
2022-07-02 20:42:11 +0900

    [feat] Configure temporary Model
    TableView test를 위한 임시 Model

--- VoiceRecorder/Model/AudioData.swift
+++ VoiceRecorder/Model/AudioData.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct AudioData {
+    
+    static var Sample = AudioData(title: "2020_07_02_16:12:03", playTime: "03:12", data: nil)
+    
+    var title: String
+    var playTime: String
+    var data: Data?
+}
