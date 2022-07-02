7972e11d65078278791ecf406c5926ce06b54aae
신의연 <dusdl14@gmail.com>
2022-07-02 20:41:02 +0900

    [feat] Configuring FireStorage Networking
    FireStorage networking 구성중
    Layer 나누는 중

--- VoiceRecorder/Network/FireStorageService.swift
+++ VoiceRecorder/Network/FireStorageService.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import FirebaseStorage
+
+protocol NetworkMethodProtocol {
+    
+    var method: StorageTaskManagement { get }
+}
+
+enum FireStorageService {
+    static let baseUrl = "gs://voicerecorder-d6222.appspot.com/"
+}
+
+extension FireStorageService {
+    enum NetworkMethodType: NetworkMethodProtocol {
+        
+        case download(StorageTaskManagement)
+        case upload(StorageTaskManagement)
+    
+        var method: StorageTaskManagement {
+            switch self {
+            case .download(let storage):
+                return storage as! StorageDownloadTask
+            case .upload(let storage):
+                return storage as! StorageUploadTask
+            }
+        }
+    }
+}
--- VoiceRecorder/Network/FirebaseStorageManager.swift
+++ VoiceRecorder/Network/FirebaseStorageManager.swift
@@ -11,23 +11,69 @@ import UIKit
 
 class FirebaseStorageManager {
     
-    static var url = "gs://voicerecorder-d6222.appspot.com/"
+    private var baseUrl: URL?
     
-
-    static func download(urlString: String, completion: @escaping (Data?) -> Void) { // 싱글톤 유무
+    init(_ url: String) {
+        self.baseUrl = URL(string: url)!
+    }
+    
+    func getAllFileList() {
+        let storageReference = Storage.storage().reference(forURL: "gs://voicerecorder-d6222.appspot.com")
+        storageReference.listAll { (result, error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            if let result = result {
+                for item in result.items {
+                    // item.reference로 파일 다운
+                    print(item)
+                }
+            }
+        }
         
-        Storage.storage().reference(forURL: "gs://voicerecorder-d6222.appspot.com/").downloadURL { url, error in
+    }
+    
+    func download(urlString: String, completion: @escaping (Data?) -> Void) { // 싱글톤 유무
+        
+        
+        
+        
+        let a = Storage.storage().reference(forURL: "gs://voicerecorder-d6222.appspot.com/").downloadURL { url, error in
             
             let data = try! Data(contentsOf: url!)
             completion(data)
         }
+        
     }
     
     
-    static func uploadAudioFile() {
-        guard let assetData = NSDataAsset.init(name: "sound")?.data else { return }
-        StorageMetadata().contentType = "mp3"
-        let audioName = UUID().uuidString + String(Date().timeIntervalSince1970)
+    func uploadAudioFile() {
+        
+         
+        let localFile = URL(string: "path/to/image")!
+        
+        
+        // Create a reference to the file you want to upload
+        let riversRef = Storage.storage().reference(forURL: "gs://voicerecorder-d6222.appspot.com/").child("images/rivers.jpg")
+      
+        // Upload the file to the path "images/rivers.jpg"
+        let uploadTask = riversRef.putFile(from: localFile, metadata: nil) { metadata, error in
+            guard let metadata = metadata else {
+                // Uh-oh, an error occurred!
+                return
+            }
+            // Metadata contains file metadata such as size, content-type.
+            let size = metadata.size
+            // You can also access to download URL after upload.
+            riversRef.downloadURL { (url, error) in
+                guard let downloadURL = url else {
+                    // Uh-oh, an error occurred!
+                    return
+                }
+            }
+        }
     }
     
     
