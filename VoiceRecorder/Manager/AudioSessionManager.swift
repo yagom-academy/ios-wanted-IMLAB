a0c0e2c30e5b1869a67f61ac4b3e300dfe74ee4c
3dots3craters <3dots3craters@gmail.com>
2022-07-02 18:01:34 +0900

    feat : add setAudioSession function
    - Manager를 통해 오디오세션 카테고리세팅 및 녹음 권한 요청 처리하도록 변경

--- VoiceRecorder/Manager/AudioSessionManager.swift
+++ VoiceRecorder/Manager/AudioSessionManager.swift
@@ -5,4 +5,24 @@
 //  Created by hayeon on 2022/07/02.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+
+class AudioSessionManager{
+    
+    let audioSession = AVAudioSession.sharedInstance()
+
+    func setAudioSession() {
+        do {
+            try audioSession.setCategory(.playAndRecord)
+            audioSession.requestRecordPermission{ accepted in
+                if accepted {
+                    print("permission granted")
+                }
+            }
+        } catch {
+            print("audioSession error: \(error.localizedDescription)")
+        }
+    }
+}
+
