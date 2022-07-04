f246477cec765db7c2e72cbb396d124e4ee66cca
iclxxud <iclxxud@gmail.com>
2022-07-04 19:59:18 +0900

    [feat] 녹음중 WaveFormView 기본 셋업 #23

---
+++ VoiceRecorder/Presentation/WaveFormView.swift
@@ -0,0 +1,52 @@
+//
+//  WaveFormView.swift
+//  VoiceRecorder
+//
+//  Created by jamescode on 2022/07/04.
+//
+
+import UIKit
+
+
+enum WaveFormViewDataType {
+    case all
+    case live
+}
+
+class WaveFormView: UIView {
+    
+    // MARK: - Properties
+    var waveFormViewDataType: WaveFormViewDataType = .live
+    var caLayer: CAShapeLayer!
+    var barWidth: CGFloat = 4.0
+    var color = UIColor.gray.cgColor
+    var waveforms = [Float]()
+    var count = 0
+    
+    //활성화에 따른 바 색상 변경
+    var active = false {
+        didSet {
+            if self.active {
+                self.color = UIColor.red.cgColor
+            }
+            else {
+                self.color = UIColor.gray.cgColor
+            }
+        }
+    }
+    
+    // MARK: - init
+    override init (frame : CGRect) {
+        super.init(frame : frame)
+        self.backgroundColor = UIColor.clear
+        caLayer = CAShapeLayer()
+        caLayer.strokeColor = UIColor.red.cgColor
+        caLayer.lineWidth = 1
+        caLayer.cornerRadius = 0.5
+        self.layer.addSublayer(caLayer)
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+    } 
+}
