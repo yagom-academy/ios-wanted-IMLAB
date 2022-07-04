38404d1131267cbe65ead31d997317647a2ada05
hhhan0315 <hhan0315@naver.com>
2022-07-04 16:09:06 +0900

    <feat> 녹음 파일 삭제 AlertController 구현으로 확인, 취소 선택 가능

--- VoiceRecorder/VIew/HomeViewController.swift
+++ VoiceRecorder/VIew/HomeViewController.swift
@@ -20,65 +20,68 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBackground
         
-        self.configure()
+        configure()
         
-        self.viewModel.loadingEnded = { [weak self] in
+        viewModel.loadingEnded = { [weak self] in
             self?.tableView.reloadData()
         }
         
-        self.viewModel.fetch()
+        viewModel.fetch()
     }
 }
 
 private extension HomeViewController {
     func configure() {
-        self.configureNavigation()
-        self.addSubViews()
-        self.makeConstraints()
+        configureView()
+        configureNavigation()
+        addSubViews()
+        makeConstraints()
+    }
+    
+    func configureView() {
+        view.backgroundColor = .systemBackground
     }
     
     func configureNavigation() {
-        self.navigationItem.title = "Voice Memos"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(touchAddButton))
+        navigationItem.title = "Voice Memos"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(touchAddButton))
     }
     
     func addSubViews() {
-        
         [tableView].forEach{
             view.addSubview($0)
         }
     }
     
     func makeConstraints() {
-        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
         ])
     }
     
     @objc func touchAddButton() {
         let recordController = RecordViewController()
         recordController.delegate = self
-        self.present(recordController, animated: true)
+        present(recordController, animated: true)
     }
 }
 
 // MARK: - UITableViewDataSource
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.viewModel.audiosCount()
+        return viewModel.audiosCount()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identfier, for: indexPath) as? HomeTableViewCell else {
             return UITableViewCell()
         }
-        let audio = self.viewModel.audio(at: indexPath.row)
+        let audio = viewModel.audio(at: indexPath.row)
         cell.audio = audio
         return cell
     }
@@ -88,17 +91,22 @@ extension HomeViewController: UITableViewDataSource {
 
 extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let audio = self.viewModel.audio(at: indexPath.row)
+        let audio = viewModel.audio(at: indexPath.row)
         let playViewController = PlayViewController()
         playViewController.audio = audio
-        self.present(playViewController, animated: true)
+        present(playViewController, animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            viewModel.deleteAudio(indexPath)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            let alertController = UIAlertController(title: "녹음파일 삭제", message: nil, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+            alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
+                self.viewModel.deleteAudio(indexPath)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }))
+            present(alertController, animated: true)
         }
     }
 }
