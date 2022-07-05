98258a99117449368956057d6e27f57cde45e577
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-07-05 10:17:30 +0900

    [#15] feat:tableView delegate 구현

--- VoiceRecorder/Scenes/Play/PlayViewController.swift
+++ VoiceRecorder/Scenes/Play/PlayViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class PlayViewController: UIViewController {
 
   let playView = PlayView()
-  var url: URL? = URL(string: "https://firebasestorage.googleapis.com:443/v0/b/voicerecorder-7fa55.appspot.com/o/testAudio.mp3?alt=media&token=6e64aacd-7b5f-457f-8ca3-6859cc6891d9")!
+  var url: URL?
   var audio: Audio?
 
   override func viewDidLoad() {
