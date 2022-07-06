2bcc9d9b7b053c7c44d37b387e315befa7de0bad
hhhan0315 <hhan0315@naver.com>
2022-07-06 13:35:32 +0900

    <fix> FirebaseStorageManager 코드 간소화

--- VoiceRecorder/Service/FirebaseStorageManager.swift
+++ VoiceRecorder/Service/FirebaseStorageManager.swift
@@ -5,16 +5,14 @@
 //  Created by rae on 2022/06/27.
 //
 
-import Foundation
 import FirebaseStorage
 import UIKit
-import AVFAudio
-import Combine
 
 class FirebaseStorageManager {
     static let shared = FirebaseStorageManager()
-    private let storage = Storage.storage().reference().child("voiceRecords")
-    private let deviceId:String?
+    private let folderName = "voiceRecords"
+    private lazy var storage = Storage.storage().reference().child(folderName)
+    private let deviceId: String?
     
     private init() {
         self.deviceId = UIDevice.current.identifierForVendor?.uuidString
@@ -22,24 +20,49 @@ class FirebaseStorageManager {
     
     func fetch(completion: @escaping (Result<Audio, Error>) -> Void) {
         
-        if let deviceId = deviceId {
-            storage.child(deviceId).listAll { result in
-                switch result{
-                case .success(let result):
-                    DispatchQueue.main.async {
-                        result.items.forEach { item in
-                            let title = "voiceRecords_" + item.name.replacingOccurrences(of: ".m4a", with: "")
-                            
-                            let fileURL = URL(fileURLWithPath: title, isDirectory: false, relativeTo: URL(fileURLWithPath: NSTemporaryDirectory()))
-                            item.write(toFile: fileURL) { [weak self] url, err in
-                                guard let self = self,
-                                      err == nil else{return}
-                                if let url = url{
-                                    let audio = Audio(title: title, url: url, fileName: item.name)
-                                    completion(.success(audio))
-                                }
-                            }
+        guard let deviceId = deviceId else {
+            return
+        }
+
+        storage.child(deviceId).listAll { result in
+            switch result {
+            case .success(let result):
+                result.items.forEach { item in
+                    let title = "\(self.folderName)_" + item.name.replacingOccurrences(of: ".m4a", with: "")
+                    item.downloadURL { result in
+                        switch result {
+                        case .success(let url):
+                            let audio = Audio(title: title, url: url, fileName: item.name)
+                            print(audio.title, audio.fileName)
+                            completion(.success(audio))
+                        case .failure(let error):
+                            completion(.failure(error))
+                        }
                     }
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        
+//        if let deviceId = deviceId {
+//            storage.child(deviceId).listAll { result in
+//                switch result{
+//                case .success(let result):
+//                    DispatchQueue.main.async {
+//                        result.items.forEach { item in
+//                            let title = "voiceRecords_" + item.name.replacingOccurrences(of: ".m4a", with: "")
+//
+//                            let fileURL = URL(fileURLWithPath: title, isDirectory: false, relativeTo: URL(fileURLWithPath: NSTemporaryDirectory()))
+//                            item.write(toFile: fileURL) { [weak self] url, err in
+//                                guard let self = self,
+//                                      err == nil else{return}
+//                                if let url = url{
+//                                    let audio = Audio(title: title, url: url, fileName: item.name)
+//                                    completion(.success(audio))
+//                                }
+//                            }
+//                    }
                     
                         //TODO: - item을 로컬로 다운로드 하고, 캐싱 처리하기
 //                        item.write(toFile: URL(string: title)) { url, error in
@@ -54,15 +77,17 @@ class FirebaseStorageManager {
 //                                completion(.success(audio))
 //                            }
 //                        }
-                    }
-                case let .failure(err):
-                    print("Error in fetch FirebaseManager \(err.localizedDescription)")
-                }
-            }
-        }
+//                    }
+//                case let .failure(err):
+//                    print("Error in fetch FirebaseManager \(err.localizedDescription)")
+//                }
+//            }
+//        }
     }
     
-    func uploadData(url: URL,fileName: String) {
+    // upload는 fileName, delete는 title -> 둘이 통합 가능할지 고민
+    
+    func uploadData(url: URL, fileName: String) {
         if let deviceId = deviceId {
             storage.child("\(deviceId)/\(fileName)").putFile(from: url)
         }
@@ -71,9 +96,10 @@ class FirebaseStorageManager {
     func deleteData(title: String) {
         if let deviceId = deviceId {
             storage.child("\(deviceId)/\(title)").delete { err in
-                if err != nil{
-                    print(err?.localizedDescription)
+                guard let err = err else {
+                    return
                 }
+                print(err.localizedDescription)
             }
         }
     }
