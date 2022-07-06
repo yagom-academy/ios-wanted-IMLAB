412b0d8170b81f10f18209eca9949f5eeef8696e
3dots3craters <3dots3craters@gmail.com>
2022-07-06 09:44:11 +0900

    chore : add VerticalLineView, WaveFormImageView files

---
+++ VoiceRecorder/View/WaveFormImageView.swift
@@ -0,0 +1,20 @@
+//
+//  WaveFormImageView.swift
+//  VoiceRecorder
+//
+//  Created by hayeon on 2022/07/06.
+//
+
+import UIKit
+
+class WaveFormImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
