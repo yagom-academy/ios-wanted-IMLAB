f3a2d4798d5f21cf2442292a6d15269d40dadce3
chansenbbai <chanchu@hansung.ac.kr>
2022-07-06 13:59:09 +0900

    [#14] 리스트뷰 UI 작업
    ### Description
    
    - 테이블뷰 UI 작업
    
    Resolve #14

--- VoiceRecorder/Presentation/AudioList/AudioListView.swift
+++ VoiceRecorder/Presentation/AudioList/AudioListView.swift
@@ -9,6 +9,16 @@ import UIKit
 
 final class AudioListView: UIView {
     
+    lazy var tableView: UITableView = {
+        lazy var tableView = UITableView()
+        
+        tableView.register(AudioListTableViewCell.self, forCellReuseIdentifier: AudioListTableViewCell.identifier)
+        tableView.separatorInset.left = 15
+        tableView.separatorInset.right = 15
+        
+        return tableView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -21,10 +31,16 @@ final class AudioListView: UIView {
     }
     
     func setupView() {
-        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(tableView)
     }
     
     func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+             tableView.topAnchor.constraint(equalTo: self.topAnchor),
+             tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+             tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+             tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+           ])
     }
 }
