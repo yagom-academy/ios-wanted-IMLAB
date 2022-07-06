349ed2dc19c2ff230496292a7469f0838e086ec1
yoonju lee <rsyjlee@gmail.com>
2022-07-06 10:13:57 +0900

    [#13] AudioRepository 타입 구현
    - 기존의 FirebaseRepositoryInterface를 범용성있게 수정

---
+++ VoiceRecorder/Domain/Inteface/Repository/AudioRepository.swift
@@ -0,0 +1,18 @@
+//
+//  AudioRepository.swift
+//  VoiceRecorder
+//
+//  Created by 이윤주 on 2022/07/06.
+//
+
+import Foundation
+
+protocol AudioRepository {
+
+    associatedtype EndPoint
+
+    func fetchAll() async throws -> [EndPoint]
+    func download(from endPoint: EndPoint) async throws -> Data
+    func upload() -> EndPoint
+    func delete(_ endPoint: EndPoint)
+}
