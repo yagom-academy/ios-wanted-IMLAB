2bb8c5584ff1c985bf0d85162edd876a1c308f78
Kim-Junhwan <tazanboy1@naver.com>
2022-07-07 20:29:46 +0900

    refactor : separation Extension
    -Extension 분리

--- VoiceRecorder/Controller/PlayVoiceViewController.swift
+++ VoiceRecorder/Controller/PlayVoiceViewController.swift
@@ -268,36 +268,7 @@ extension PlayVoiceViewController : PlayVoiceDelegate{
     }
 }
 
-extension UIImageView{
-    func load(url : URL , completion : @escaping ()->Void){
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url){
-                if let image = UIImage(data: data){
-                    DispatchQueue.main.async {
-                        let resizedImage = image.aspectFitImage(inRect: self?.bounds ?? CGRect(x: 0, y: 0, width: 10, height: 10))
-                        self?.image = resizedImage
-                        completion()
-                    }
-                }
-            }
-        }
-    }
-}
 
 
-extension UIImage {
-    func aspectFitImage(inRect rect: CGRect) -> UIImage? {
-        let width = self.size.width
-        let height = self.size.height
-        let scaleFactor = rect.size.height / height
 
-        UIGraphicsBeginImageContext(CGSize(width: width * scaleFactor, height: height * scaleFactor))
-        self.draw(in: CGRect(x: 0.0, y: 0.0, width: width * scaleFactor, height: height * scaleFactor))
 
-        defer {
-            UIGraphicsEndImageContext()
-        }
-
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
-}
--- VoiceRecorder/Controller/RecordVoiceViewController.swift
+++ VoiceRecorder/Controller/RecordVoiceViewController.swift
@@ -366,31 +366,5 @@ extension RecordVoiceViewController : PlayVoiceDelegate{
     }
 }
 
-extension TimeInterval{
 
-    func getStringTimeInterval() -> String {
 
-        let seconds = self
-        let hour = Int(seconds) / (60 * 60)
-        let min = Int(seconds) / 60
-        let sec = Int(seconds) % 60
-        let cen = Int(seconds * 100) % 100
-        
-        if hour == 0{
-            let formatString = "%0.2d:%0.2d:%0.2d"
-            return String(format: formatString, min, sec, cen)
-        }else{
-            let formatString = "%0.2d:%0.2d:%0.2d"
-            return String(format: formatString, hour, min, sec)
-        }
-        
-    }
-}
-
-extension String {
-    subscript(_ range: Range<Int>) -> String {
-        let fromIndex = self.index(self.startIndex, offsetBy: range.startIndex)
-        let toIndex = self.index(self.startIndex,offsetBy: range.endIndex)
-        return String(self[fromIndex..<toIndex])
-    }
-}
