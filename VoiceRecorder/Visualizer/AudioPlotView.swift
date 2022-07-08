60e86b4362cc3630fd217a740a95b30854cfa31a
신의연 <dusdl14@gmail.com>
2022-07-08 17:07:43 +0900

    [feat] 사운드 visualizer 구현

--- VoiceRecorder/Visualizer/AudioPlotView.swift
+++ VoiceRecorder/Visualizer/AudioPlotView.swift
@@ -2,7 +2,120 @@
 //  AudioPlotView.swift
 //  VoiceRecorder
 //
-//  Created by 신의연 on 2022/07/08.
+//  Created by 신의연 on 2022/07/07.
 //
 
-import Foundation
+import UIKit
+
+protocol a {
+    func d()
+}
+
+class AudioPlotView: UIView {
+    
+    var delegate: a!
+    
+    var caLayer: CAShapeLayer!
+    // MARK: - Vars
+    
+    /// Bar width
+    var barWidth: CGFloat = 4.0
+    
+    /// Indicate that waveform should draw active/inactive state
+    var active = false {
+        didSet {
+            if self.active {
+                self.color = UIColor.red.cgColor
+            }
+            else {
+                self.color = UIColor.gray.cgColor
+            }
+        }
+    }
+    
+    /// Color for bars
+    var color = UIColor.gray.cgColor
+    
+    /// Given waveforms
+    var waveforms = [Int]()
+    var count = 0
+    
+    // MARK: - Init
+    
+    override init (frame : CGRect) {
+        super.init(frame : frame)
+        self.backgroundColor = UIColor.clear
+        caLayer = CAShapeLayer()
+        caLayer.strokeColor = UIColor.red.cgColor
+        caLayer.lineWidth = 1
+        caLayer.cornerRadius = 0.5
+        self.layer.addSublayer(caLayer)
+        self.transform = CGAffineTransform(scaleX: -1, y: 1)
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+        caLayer = CAShapeLayer()
+        caLayer.strokeColor = UIColor.red.cgColor
+        caLayer.lineWidth = 1
+        caLayer.cornerRadius = 0.5
+        self.layer.addSublayer(caLayer)
+        self.transform = CGAffineTransform(scaleX: -1, y: 1)
+    }
+        
+    public func shiftWaveform(value: CGFloat) {
+        guard let sublayers = self.layer.sublayers else { return }
+        for layer in sublayers {
+            layer.transform = CATransform3DTranslate(layer.transform, 0, 0, 0)
+            let transform  = CATransform3DTranslate(layer.transform, value, 0, 0)
+            layer.transform = transform
+        }
+    }
+    public func shiftWaveform() {
+        guard let sublayers = self.layer.sublayers else { return }
+        for layer in sublayers {
+            let transform  = CATransform3DTranslate(layer.transform, -5, 0, 0)
+            layer.transform = transform
+        }
+        
+    }
+    func shiftBackward(value: CGFloat) {
+        guard let sublayers = self.layer.sublayers else { return }
+        for layer in sublayers {
+            layer.transform = CATransform3DTranslate(layer.transform, 0, 0, 0)
+            let transform  = CATransform3DTranslate(layer.transform, value, 0, 0)
+            layer.transform = transform
+        }
+        
+    }
+    func move() {
+        caLayer.scroll(CGPoint(x: 100, y: 300))
+    }
+    override func draw(_ rect: CGRect) {
+        shiftWaveform()
+        count += 1
+    
+        let path: UIBezierPath!
+        
+        if let ppath = caLayer.path {
+            path = UIBezierPath(cgPath: ppath)
+        } else {
+            path = UIBezierPath()
+        }
+    
+        guard var wave = waveforms.last else { return }
+        
+        if (Int(wave) <= 2) {
+            wave = 2
+        }
+        
+        let startX = Int(self.bounds.width/2) + 5*count
+        let startY = Int(self.bounds.origin.y) + Int(self.bounds.height)/2
+        
+        path.move(to: CGPoint(x: startX, y: startY + wave))
+        path.addLine(to: CGPoint(x: startX, y: startY - wave))
+      
+        caLayer.path = path.cgPath
+    }
+    
+}
--- VoiceRecorder/Visualizer/AudioVisualizeView.swift
+++ VoiceRecorder/Visualizer/AudioVisualizeView.swift
@@ -6,7 +6,78 @@
 //
 
 import UIKit
+import AVFAudio
+import Accelerate
 
-class AudioPlotView: UIScrollView {
+class AudioVisualizeView: UIScrollView {
+    
+    private var audioPlotView: AudioPlotView = {
+        var audioPlotView = AudioPlotView()
+        audioPlotView.translatesAutoresizingMaskIntoConstraints = false
+        return audioPlotView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        self.indicatorStyle = .white
+        self.backgroundColor = .black
+        self.showsVerticalScrollIndicator = false
+        self.showsHorizontalScrollIndicator = false
+        self.transform = CGAffineTransform(scaleX: -1, y: 1)
+        setAudioPlotView()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.indicatorStyle = .white
+        self.backgroundColor = .black
+        self.showsVerticalScrollIndicator = false
+        self.showsHorizontalScrollIndicator = false
+        self.transform = CGAffineTransform(scaleX: -1, y: 1)
+        setAudioPlotView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setAudioPlotView()
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setAudioPlotView() {
+        self.addSubview(audioPlotView)
+        NSLayoutConstraint.activate([
+            
+            audioPlotView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            audioPlotView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            audioPlotView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            audioPlotView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            audioPlotView.heightAnchor.constraint(equalTo: self.heightAnchor)
+        
+        ])
+    }
+    
+    private func rms(data: UnsafeMutablePointer<Float>, frameLength: UInt) -> Float {
+        var val : Float = 0
+        vDSP_measqv(data, 1, &val, frameLength)
+        val *= 1000
+        return val
+    }
+    
+    func processAudioData(buffer: AVAudioPCMBuffer) {
+        guard let channelData = buffer.floatChannelData?[0] else {return}
+        let frames = buffer.frameLength
+        
+        let rmsValue = rms(data: channelData, frameLength: UInt(frames))
+        audioPlotView.waveforms.append(Int(rmsValue))
+        DispatchQueue.main.async { [self] in
+            self.audioPlotView.setNeedsDisplay()
+        }
+    }
+    func udpateVisualizerContentSize() {
+        self.contentSize.width += 5
+    }
+    func getWaveformData() -> [Int] {
+        return audioPlotView.waveforms
+    }
     
 }
