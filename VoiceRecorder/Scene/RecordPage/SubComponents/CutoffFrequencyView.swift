169b2fabfc6824b362813bbadd0aaa1abafcfc35
kimkirim <kirimrlfla@naver.com>
2022-07-08 16:53:58 +0900

    merge:conflict누락 수정

--- VoiceRecorder/Scene/RecordPage/SubComponents/CutoffFrequencyView.swift
+++ VoiceRecorder/Scene/RecordPage/SubComponents/CutoffFrequencyView.swift
@@ -13,41 +13,36 @@ class CutoffFrequencyView: UIView {
         label.text = "cutoff freqency"
         label.font = .systemFont(ofSize: 18, weight: .semibold)
         label.textColor = .label
-        
+
         return label
     }()
-    
+
     private let frequencySlider: UISlider = {
         let slider = UISlider()
-<<<<<<< HEAD
-        
-=======
-
->>>>>>> feature-record
         slider.minimumValue = 0.0
         slider.maximumValue = 60.0
-        
+
         slider.value = 60.0
-        
+
         slider.addTarget(self, action: #selector(didChangeSlider(_:)), for: .valueChanged)
-        
+
         return slider
     }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         layout()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: - Action
     @objc func didChangeSlider(_ sender: UISlider) {
         let cutValue = sender.value
-        
+
         NotificationCenter.default.post(name: Notification.Name("SendCutValue"), object: cutValue, userInfo: nil)
     }
 }
@@ -61,12 +56,12 @@ extension CutoffFrequencyView {
             addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
-        
+
         let inset: CGFloat = 30.0
-        
+
         cutoffLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: inset).isActive = true
         cutoffLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: inset).isActive = true
-        
+
         frequencySlider.topAnchor.constraint(equalTo: cutoffLabel.bottomAnchor, constant: 8).isActive = true
         frequencySlider.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: inset).isActive = true
         frequencySlider.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -inset).isActive = true
