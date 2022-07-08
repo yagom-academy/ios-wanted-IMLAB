bdd13f7ccfb18d7e2b792df7c9bd278398db12ff
신의연 <dusdl14@gmail.com>
2022-07-08 17:07:01 +0900

    [feat] visualizer 붙임

--- VoiceRecorder/RecordVoice/VC/RecordViewController.swift
+++ VoiceRecorder/RecordVoice/VC/RecordViewController.swift
@@ -1,155 +1,9 @@
-////
-////  RecordViewController.swift
-////  VoiceRecorder
-////
-////  Created by 백유정 on 2022/07/04.
-////
 //
-//import UIKit
-//import AVFoundation
+//  RecordViewController.swift
+//  VoiceRecorder
 //
-//class RecordViewController: UIViewController {
-//    
-//    var soundManager = SoundManager()
-//    var audioFileManager = AudioFileManager()
-//    var firebaseStorageManager = FirebaseStorageManager()
-//    var engine = AVAudioEngine()
-//    
-//    var isStartRecording: Bool = false
-//    
-//    var recordButton: UIButton = {
-//        var button = UIButton()
-//        let largeConfig = UIImage.SymbolConfiguration(pointSize: 50, weight: .regular, scale: .large)
-//        let largeRecordImage = UIImage(systemName: "circle.fill", withConfiguration: largeConfig)
-//        button.setImage(largeRecordImage, for: .normal)
-//        button.tintColor = .systemRed
-//        return button
-//    }()
-//    
-//    private lazy var playControlView: PlayControlView = {
-//        var view = PlayControlView()
-//        view.delegate = self
-//        return view
-//    }()
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        setLayout()
-//        setAudio()
-//        
-//        recordButton.addTarget(self, action: #selector(control), for: .touchUpInside)
-//    }
-//    
-//    func setLayout() {
-//        view.backgroundColor = .white
-//        
-//        recordButton.translatesAutoresizingMaskIntoConstraints = false
-//        playControlView.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        view.addSubview(recordButton)
-//        view.addSubview(playControlView)
-//        
-//        NSLayoutConstraint.activate([
-//            recordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            recordButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
-//            recordButton.widthAnchor.constraint(equalToConstant: 80),
-//            recordButton.heightAnchor.constraint(equalToConstant: 80),
-//            
-//            playControlView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            playControlView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            playControlView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-//            playControlView.heightAnchor.constraint(equalToConstant: 100)
-//        ])
-//    }
-//    
-//    func recordButtonToggle() {
-//        let largeConfig = UIImage.SymbolConfiguration(pointSize: 50, weight: .regular, scale: .large)
-//        let largeRecordImage = UIImage(systemName: "circle.fill", withConfiguration: largeConfig)
-//        let largePauseImage = UIImage(systemName: "square.circle", withConfiguration: largeConfig)
-//        
-//        let image = self.isStartRecording ? largePauseImage : largeRecordImage
-//        self.recordButton.setImage(image, for: .normal)
-//    }
+//  Created by 백유정 on 2022/07/04.
 //
-<<<<<<< HEAD
-//    func setAudio() {
-//        requestMicrophoneAccess { [weak self] allowed in
-//            if allowed {
-//                // 녹음 권한 허용
-//                let format = self?.engine.inputNode.outputFormat(forBus: 0)
-//                self?.soundManager.configureRecordEngine(format: format!)
-//            } else {
-//                // 녹음 권한 거부
-//                fatalError()
-//            }
-//        }
-//    }
-//    
-//    // 녹음 시작 & 정지 컨트롤
-//    @objc private func control() {
-//        isStartRecording = !isStartRecording
-//        recordButtonToggle()
-//        
-//        let url = audioFileManager.getAudioFilePath(fileName: "fileNAME")
-//        if isStartRecording { // 녹음 시작일 때
-//            soundManager.startRecord(filePath: url)
-//        } else { // 녹음 끝일 때
-//            soundManager.stopRecord()
-//            firebaseStorageManager.uploadAudio(url: url)
-//            soundManager.initializedEngine(url: url)
-//            print(soundManager.totalPlayTime(), "시간")
-//        }
-//    }
-//}
-//
-//extension RecordViewController {
-//    
-//    // 마이크 접근 권한 요청
-//    private func requestMicrophoneAccess(completion: @escaping (Bool) -> Void) {
-//        do {
-//            let recordingSession: AVAudioSession = AVAudioSession.sharedInstance()
-//            switch recordingSession.recordPermission {
-//            case .undetermined: // 아직 녹음 권한 요청이 되지 않음, 사용자에게 권한 요청
-//                recordingSession.requestRecordPermission({ allowed in // bool 값
-//                    completion(allowed)
-//                })
-//            case .denied: // 사용자가 녹음 권한 거부, 사용자가 직접 설정 화면에서 권한 허용을 하게끔 유도
-//                print("[Failure] Record Permission is Denied.")
-//                completion(false)
-//            case .granted: // 사용자가 녹음 권한 허용
-//                print("[Success] Record Permission is Granted.")
-//                completion(true)
-//            @unknown default:
-//                fatalError("[ERROR] Record Permission is Unknown Default.")
-//            }
-//        }
-//    }
-//}
-//
-//extension RecordViewController: SoundButtonActionDelegate {
-//    
-//    func playButtonTouchUpinside(sender: UIButton) {
-//        if sender.isSelected {
-//            soundManager.pause()
-//        } else {
-//            soundManager.play()
-//        }
-//    }
-//    
-//    func backwardButtonTouchUpinside(sender: UIButton) {
-//        print("backwardButton Clicked")
-//        
-//        soundManager.seek(to: true)
-//    }
-//    
-//    func forwardTouchUpinside(sender: UIButton) {
-//        print("forwardButton Clicked")
-//        
-//        soundManager.seek(to: false)
-//    }
-//}
-=======
 
 import UIKit
 import AVFoundation
@@ -159,6 +13,7 @@ class RecordViewController: UIViewController {
     var soundManager = SoundManager()
     var audioFileManager = AudioFileManager()
     var firebaseStorageManager = FirebaseStorageManager()
+    var date = DateUtil().formatDate()
     var engine = AVAudioEngine()
     
     var isStartRecording: Bool = false
@@ -172,6 +27,12 @@ class RecordViewController: UIViewController {
         return button
     }()
     
+    private var visualizer: AudioVisualizeView = {
+        var visualizer = AudioVisualizeView()
+        visualizer.translatesAutoresizingMaskIntoConstraints = false
+        return visualizer
+    }()
+    
     private lazy var playControlView: PlayControlView = {
         var view = PlayControlView()
         view.delegate = self
@@ -183,7 +44,7 @@ class RecordViewController: UIViewController {
         
         setLayout()
         setAudio()
-        
+        soundManager.visualDelegate = self
         recordButton.addTarget(self, action: #selector(control), for: .touchUpInside)
     }
     
@@ -195,8 +56,15 @@ class RecordViewController: UIViewController {
         
         view.addSubview(recordButton)
         view.addSubview(playControlView)
+        view.addSubview(visualizer)
         
         NSLayoutConstraint.activate([
+            
+            visualizer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            visualizer.centerYAnchor.constraint(equalTo: view.centerYAnchor).constraintWithMultiplier(0.5),
+            visualizer.heightAnchor.constraint(equalToConstant: 200),
+            visualizer.widthAnchor.constraint(equalTo: view.widthAnchor),
+            
             recordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             recordButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
             recordButton.widthAnchor.constraint(equalToConstant: 80),
@@ -217,7 +85,7 @@ class RecordViewController: UIViewController {
         let image = self.isStartRecording ? largePauseImage : largeRecordImage
         self.recordButton.setImage(image, for: .normal)
     }
-
+    
     func setAudio() {
         requestMicrophoneAccess { [weak self] allowed in
             if allowed {
@@ -236,13 +104,13 @@ class RecordViewController: UIViewController {
         isStartRecording = !isStartRecording
         recordButtonToggle()
         
-        let url = audioFileManager.getAudioFilePath(fileName: "fileNAME")
+        let url = audioFileManager.getAudioFilePath(fileName: date+".caf")
         if isStartRecording { // 녹음 시작일 때
             soundManager.startRecord(filePath: url)
         } else { // 녹음 끝일 때
             soundManager.stopRecord()
             firebaseStorageManager.uploadAudio(url: url)
-            soundManager.initializedEngine(url: url)
+            soundManager.initializeSoundManager(url: url, type: .playBack)
         }
     }
 }
@@ -274,23 +142,25 @@ extension RecordViewController {
 extension RecordViewController: SoundButtonActionDelegate {
     
     func playButtonTouchUpinside(sender: UIButton) {
-        if sender.isSelected {
-            soundManager.pause()
-        } else {
-            soundManager.play()
-        }
+        self.soundManager.playNpause()
     }
     
     func backwardButtonTouchUpinside(sender: UIButton) {
         print("backwardButton Clicked")
-        
-        soundManager.seek(to: true)
+        soundManager.skip(forwards: false)
     }
     
     func forwardTouchUpinside(sender: UIButton) {
         print("forwardButton Clicked")
-        
-        soundManager.seek(to: false)
+        soundManager.skip(forwards: true)
     }
 }
->>>>>>> 0c3f8de95a603b8f9fc83406dc171708115dcdc8
+
+extension RecordViewController: Visualizerable {
+    func processAudioBuffer(buffer: AVAudioPCMBuffer) {
+
+        visualizer.processAudioData(buffer: buffer)
+        
+    }
+    
+}
