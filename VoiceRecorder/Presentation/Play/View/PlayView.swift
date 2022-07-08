bc6b51477b47c9f6451f9ef011200e735cdadab8
oguuk <ogw135@gmail.com>
2022-07-08 17:39:02 +0900

    [#21] PlayView에 volumeLabel 추가

--- VoiceRecorder/Presentation/Play/View/PlayView.swift
+++ VoiceRecorder/Presentation/Play/View/PlayView.swift
@@ -10,30 +10,37 @@ import UIKit
 final class PlayView: UIView, ViewPresentable {
     
     private let titleLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.font = UIFont.preferredFont(forTextStyle: .body)
         return label
     }()
     
-     let segmentedContoller: UISegmentedControl = {
+    private let volumeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "volume"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let segmentedContoller: UISegmentedControl = {
         let segment = UISegmentedControl(items: ["일반 목소리","아기 목소리","할아버지 목소리"])
         return segment
     }()
     
-     let goforward5Button:UIButton = {
+    let goforward5Button:UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "goforward.5"), for: .normal)
         return button
     }()
     
-     let goBackrward5Button:UIButton = {
+    let goBackrward5Button:UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "gobackward.5"), for: .normal)
         return button
     }()
     
     private let playButtonHorizontalStackView : UIStackView = {
-       let stackView = UIStackView()
+        let stackView = UIStackView()
         stackView.distribution = .fillEqually
         stackView.axis = .horizontal
         stackView.alignment = .center
@@ -42,20 +49,20 @@ final class PlayView: UIView, ViewPresentable {
     
     private let verticalStackView : UIStackView = {
         let stackView = UIStackView()
-        stackView.spacing = 20
-         stackView.distribution = .fillEqually
-         stackView.axis = .vertical
-         stackView.alignment = .center
-         return stackView
+        stackView.spacing = 30
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        return stackView
     }()
     
-     let startButton : UIButton = {
+    let startButton : UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "play"), for: .normal)
         return button
     }()
     
-     let slider : UISlider = {
+    let slider : UISlider = {
         let slider = UISlider()
         slider.minimumValue = 0
         slider.maximumValue = 10
@@ -73,8 +80,11 @@ final class PlayView: UIView, ViewPresentable {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-
+    
     func setupView() {
+        addSubview(volumeLabel)
+        volumeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         [titleLabel,verticalStackView].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             addSubview($0)
@@ -84,7 +94,7 @@ final class PlayView: UIView, ViewPresentable {
             $0.translatesAutoresizingMaskIntoConstraints = false
             verticalStackView.addArrangedSubview($0)
         }
-    
+        
         [goBackrward5Button,startButton,goforward5Button].forEach {
             playButtonHorizontalStackView.addArrangedSubview($0)
         }
@@ -95,9 +105,12 @@ final class PlayView: UIView, ViewPresentable {
         titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         
-        slider.leadingAnchor.constraint(equalTo: verticalStackView.leadingAnchor).isActive = true
-        slider.trailingAnchor.constraint(equalTo: verticalStackView.trailingAnchor).isActive = true
-
+        slider.leadingAnchor.constraint(equalTo: segmentedContoller.leadingAnchor).isActive = true
+        slider.trailingAnchor.constraint(equalTo: segmentedContoller.trailingAnchor).isActive = true
+        
+        volumeLabel.leadingAnchor.constraint(equalTo: slider.leadingAnchor).isActive = true
+        volumeLabel.bottomAnchor.constraint(equalTo: slider.topAnchor, constant: 7).isActive = true
+        
         playButtonHorizontalStackView.leadingAnchor.constraint(equalTo: verticalStackView.leadingAnchor).isActive = true
         playButtonHorizontalStackView.trailingAnchor.constraint(equalTo: verticalStackView.trailingAnchor).isActive = true
         
